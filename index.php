1eab3ffa65568f19683846d7c9a4ddb550caaccd
Alexander <shagov25@gmail.com>
2024-10-12 10:15:11 +0300

    Second task ended

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
+$currentFileName = basename($_SERVER['SCRIPT_FILENAME']);
+$currentLineNumber = __LINE__;
+echo 'Имя файла: ' . $currentFileName . '<br/>'
+    .'Номер строки: ' . $currentLineNumber . '<br/>';
 
+$fish = 'Рыба';
+$human = 'человек';
+$manyLineArg = <<< LABEL
+    $fish рыбою сыта, 
+    а $human человеком
+LABEL;
+echo $manyLineArg;
